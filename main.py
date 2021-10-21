b84fa0ea8e128316298860312b88840d2d70b851
bat-py <bat-py@protonmail.com>
2021-10-22 00:52:25 +0500

    added /start command

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+
+import aiogram.types.reply_keyboard
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1018761895:AAE9zGMHZxYZlC_6kyRLAmTBC0Oubpp-QUQ'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_message(message: types.Message):
+    button1 = aiogram.types.reply_keyboard.KeyboardButton('1 bot')
+    button2 = aiogram.types.reply_keyboard.KeyboardButton('2 bot')
+    buttons = aiogram.types.reply_keyboard.ReplyKeyboardMarkup(keyboard=[[button1, button2]], resize_keyboard=True)
+
+    await message.reply("Choise your bot", reply_markup=buttons)
+
+
+@dp.message_handler(lambda message: message.text == 'tilla')
+async def send_message(message: types.Message):
+    await message.answer('Mirkosh chumo')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
